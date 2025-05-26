7f9239c36f6afd6af00368c390bf81e4498d2baf
청주는사과아님 <jbw9964@gmail.com>
2025-05-26 14:33:31 +0900

    inital commit

---
+++ src/main/java/org/leisureup/LeisureUp.java
@@ -0,0 +1,13 @@
+package org.leisureup;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class LeisureUp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeisureUp.class, args);
+    }
+
+}
---
+++ src/test/java/org/leisureup/LeisureUpTests.java
@@ -0,0 +1,13 @@
+package org.leisureup;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class LeisureUpTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
