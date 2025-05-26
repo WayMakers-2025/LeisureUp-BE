3ed02eac5fcb716df4d1afeeeda7b2e18a725002
청주는사과아님 <jbw9964@gmail.com>
2025-05-26 20:44:27 +0900

    ci: fix build.gradle

--- src/test/java/org/leisureup/LeisureUpTests.java
+++ src/test/java/org/leisureup/LeisureUpTests.java
@@ -1,13 +1,25 @@
 package org.leisureup;
 
+import lombok.extern.slf4j.*;
+import org.assertj.core.api.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.*;
+import org.springframework.modulith.core.*;
 
+@Slf4j
 @SpringBootTest
 class LeisureUpTests {
 
     @Test
     void contextLoads() {
+        ApplicationModules applicationModules = ApplicationModules.of(LeisureUp.class);
+
+        applicationModules.forEach(m -> log.info("Module: {}", m));
+
+        applicationModules.verify();
+
+        Assertions.assertThat(false).isTrue();
     }
 
 }
