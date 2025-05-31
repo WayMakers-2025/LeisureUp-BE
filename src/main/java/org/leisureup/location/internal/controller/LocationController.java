4c36a6beb35a8ca014f93298c3a27e4b60be8b0d
청주는사과아님 <jbw9964@gmail.com>
2025-05-31 17:33:46 +0900

    setup: 도메인 + Modulith 기본 셋업

---
+++ src/main/java/org/leisureup/location/internal/controller/LocationController.java
@@ -0,0 +1,8 @@
+package org.leisureup.location.internal.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LocationController {
+
+}
---
+++ src/main/java/org/leisureup/travel/spi/package-info.java
@@ -0,0 +1,4 @@
+@NamedInterface(name = "spi")
+package org.leisureup.travel.spi;
+
+import org.springframework.modulith.*;
