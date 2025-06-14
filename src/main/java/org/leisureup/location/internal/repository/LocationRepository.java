2bbab467231c948d997bd0bc1f07488cfd92e6d6
청주는사과아님 <jbw9964@gmail.com>
2025-06-14 18:13:07 +0900

    feat: 장소 조회 서비스, endpoint 완성

--- src/main/java/org/leisureup/location/internal/repository/LocationRepository.java
+++ src/main/java/org/leisureup/location/internal/repository/LocationRepository.java
@@ -1,8 +1,15 @@
 package org.leisureup.location.internal.repository;
 
+import java.util.*;
 import org.leisureup.location.internal.domain.*;
 import org.springframework.data.jpa.repository.*;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
+    @Query("""
+            select l from Location l
+            right join fetch l.locationCategory
+            where l.id = :id
+            """)
+    Optional<Location> findByIdFetchingCategory(Long id);
 }
