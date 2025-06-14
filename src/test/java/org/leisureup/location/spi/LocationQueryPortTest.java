c8daadd104edd2d1aec4b98af8727942048c9181
청주는사과아님 <jbw9964@gmail.com>
2025-06-14 22:07:45 +0900

    feat: LocationQueryPort 구현체 구성
    +test: 테스트 코드 구성

---
+++ src/test/java/org/leisureup/location/spi/LocationQueryPortTest.java
@@ -0,0 +1,90 @@
+package org.leisureup.location.spi;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.*;
+import java.util.*;
+import java.util.stream.*;
+import lombok.extern.slf4j.*;
+import org.junit.jupiter.api.*;
+import org.leisureup.*;
+import org.leisureup.global.exception.*;
+import org.leisureup.location.internal.domain.*;
+import org.leisureup.location.internal.repository.*;
+import org.springframework.beans.factory.annotation.*;
+
+@Slf4j
+class LocationQueryPortTest extends IntegrationTestSupport {
+
+    private static final Random random = new Random();
+    private static final List<Long> testIds = LongStream.rangeClosed(1, 10)
+            .boxed().toList();
+    @Autowired
+    LocationQueryPort port;
+    @Autowired
+    LocationRepository locationRepo;
+    @Autowired
+    CategoryRepository categoryRepo;
+
+    private static Location genEntity(Long id) {
+        String title = String.valueOf(id);
+        GpsCord cord = GpsCord.of(random.nextDouble(), random.nextDouble());
+
+        Location loc = Location.of(
+                id, title, cord, null, null
+        );
+
+        loc.synchronizeTo(LocalDateTime.now());
+
+        return loc;
+    }
+
+    @BeforeEach
+    void setUp() {
+
+        Category sampleCat = CatOther.of("sample", "1234");
+        categoryRepo.save(sampleCat);
+
+        List<Location> locations = testIds.stream()
+                .map(LocationQueryPortTest::genEntity)
+                .toList();
+
+        locations.forEach(l -> l.changeCategory(sampleCat));
+        locationRepo.saveAll(locations);
+    }
+
+    @AfterEach
+    void tearDown() {
+        locationRepo.deleteAll();
+        categoryRepo.deleteAll();
+    }
+
+    @Test
+    @DisplayName("ID 목록을 통해 장소 정보를 조회할 수 있다.")
+    void getLocationListById() {
+
+        List<LocationResponse> resp = port.getLocationListById(testIds);
+
+        assertThat(resp).isNotNull().hasSize(testIds.size());
+        for (var single : resp) {
+            assertThat(single).hasNoNullFieldsOrProperties();
+        }
+    }
+
+    @Test
+    @DisplayName("ID 목록 중 식별할 수 없는 장소가 존재하면 에러를 일으킨다.")
+    void missingIdExists() {
+        Long missingId1 = 12345L;
+        Long missingId2 = 54321L;
+        List<Long> ids = new ArrayList<>(testIds);
+        ids.add(missingId1);
+        ids.add(missingId2);
+
+        assertThatThrownBy(() -> port.getLocationListById(ids))
+                .isInstanceOf(NotFound.class)
+                .hasMessageContainingAll(
+                        String.valueOf(missingId1), String.valueOf(missingId2)
+                );
+
+    }
+}
