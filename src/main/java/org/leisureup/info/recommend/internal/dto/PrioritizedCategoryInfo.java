ecba227472f70f6887a285ee09a84d4a9cad3d36
청주는사과아님 <jbw9964@gmail.com>
2025-06-21 15:39:50 +0900

    feat: 사용자 질문 응답에 따라 카테고리 선별 기능 구성
    +test: 테스트 코드 구성

---
+++ src/main/java/org/leisureup/info/recommend/internal/dto/PrioritizedCategoryInfo.java
@@ -0,0 +1,19 @@
+package org.leisureup.info.recommend.internal.dto;
+
+import org.springframework.lang.*;
+
+/**
+ * @param score 높을수록 좋은거
+ */
+public record PrioritizedCategoryInfo(
+        Long categoryId,
+        String name,
+        double score
+) implements Comparable<PrioritizedCategoryInfo> {
+
+    @Override
+    public int compareTo(@NonNull PrioritizedCategoryInfo o) {
+        // 이래야 높은 점수가 head 로 정렬
+        return Double.compare(o.score, score);
+    }
+}
