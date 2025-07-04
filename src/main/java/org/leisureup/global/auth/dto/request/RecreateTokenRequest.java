3f4ed9d0534b7b1c755e5d220e58386e60e09bb2
청주는사과아님 <jbw9964@gmail.com>
2025-07-04 19:08:27 +0900

    enhance: validation error message 추가

--- src/main/java/org/leisureup/global/auth/dto/request/RecreateTokenRequest.java
+++ src/main/java/org/leisureup/global/auth/dto/request/RecreateTokenRequest.java
@@ -3,7 +3,8 @@ package org.leisureup.global.auth.dto.request;
 import jakarta.validation.constraints.*;
 
 public record RecreateTokenRequest(
-        @NotBlank String refreshToken
+        @NotBlank(message = "토큰은 비어있을 수 없습니다.")
+        String refreshToken
 ) {
 
 }
--- src/main/java/org/leisureup/member/internal/dto/request/SavePickLocationRequest.java
+++ src/main/java/org/leisureup/member/internal/dto/request/SavePickLocationRequest.java
@@ -3,7 +3,8 @@ package org.leisureup.member.internal.dto.request;
 import jakarta.validation.constraints.*;
 
 public record SavePickLocationRequest(
-        @NotNull @Positive
+        @NotNull(message = "locationId 는 비어있을 수 없습니다.")
+        @Positive(message = "locationId 는 0 보다 커야합니다.")
         Long locationId
 ) {
 
