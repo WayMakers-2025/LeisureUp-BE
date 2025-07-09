82045e700d48e2c17d8e274d896121ab5fffc9a5
청주는사과아님 <jbw9964@gmail.com>
2025-07-09 19:32:04 +0900

    feat: 레저 필터링 검색 (키워드 X) endpoint 만 구성

--- src/main/java/org/leisureup/map/internal/controller/MapController.java
+++ src/main/java/org/leisureup/map/internal/controller/MapController.java
@@ -1,14 +1,24 @@
 package org.leisureup.map.internal.controller;
 
+import jakarta.validation.*;
 import lombok.*;
+import lombok.extern.slf4j.*;
+import org.leisureup.global.exception.*;
 import org.leisureup.global.response.*;
 import org.leisureup.map.internal.dto.*;
+import org.leisureup.map.internal.dto.request.*;
+import org.leisureup.map.internal.dto.response.*;
 import org.leisureup.map.internal.service.*;
+import org.springdoc.core.annotations.*;
+import org.springframework.validation.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
+@Validated
 @RestController
 @RequiredArgsConstructor
 public class MapController {
+
     private final MapService mapService;
 
     @GetMapping("/map/search")
@@ -20,6 +30,20 @@ public class MapController {
         return ApiResponse.success(
                 200,
                 mapService.searchCategory(x, y, radius, category)
+        );
+    }
+
+    @GetMapping("/map/leisure/location-base")
+    public ApiResponse<MultiPageResponse<?>>
+    searchLeisureOnLocation(
+            @Valid @ParameterObject
+            SearchLeisureOnLocationRequest req
+    ) {
+
+        log.info("Request : {}", req.toString());
+
+        throw new NotImplemented(
+                "/map/leisure/location-base has not been implemented yet"
         );
     }
 }
