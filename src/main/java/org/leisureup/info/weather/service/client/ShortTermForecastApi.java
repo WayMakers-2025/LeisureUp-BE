d259c746e7710ea9727f2ddbf2996ae61b56499f
청주는사과아님 <jbw9964@gmail.com>
2025-07-25 17:09:33 +0900

    feat: 단기 예보 API feign client 구성

---
+++ src/main/java/org/leisureup/info/weather/service/client/ShortTermForecastApi.java
@@ -0,0 +1,30 @@
+package org.leisureup.info.weather.service.client;
+
+import org.leisureup.global.response.external.*;
+import org.leisureup.global.response.external.weather.*;
+import org.leisureup.info.weather.dto.api.*;
+import org.springframework.cloud.openfeign.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Component
+@FeignClient(
+        name = "ShortTermForecastApi",
+        url = "${feign.weather.forecast.short-term}",
+        configuration = DefaultFeignErrorDecoder.class
+)
+public interface ShortTermForecastApi {
+
+    @GetMapping("/getVilageFcst")
+    WeatherApiResponse<ShortTermForecast> getShortTermForecast(
+            @RequestParam String serviceKey,
+            @RequestParam("dataType") String respType,
+            @RequestParam("base_date") String baseDate,
+            @RequestParam("base_time") String baseTime,
+            @RequestParam int nx,
+            @RequestParam int ny,
+            @RequestParam int pageNo,
+            @RequestParam int numOfRows
+    );
+
+}
