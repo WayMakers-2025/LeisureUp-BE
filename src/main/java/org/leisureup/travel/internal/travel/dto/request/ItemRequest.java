4911314a9b243fdae6105657ce184f7ff5bf88c1
bbungjin <leejintwo12@naver.com>
2025-08-21 19:01:32 +0900

    feat: 여행 날짜 역직렬화

--- src/main/java/org/leisureup/travel/internal/travel/dto/request/ItemRequest.java
+++ src/main/java/org/leisureup/travel/internal/travel/dto/request/ItemRequest.java
@@ -1,8 +1,10 @@
 package org.leisureup.travel.internal.travel.dto.request;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.leisureup.global.json.FlexibleLocalTimeDeserializer;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -13,7 +15,9 @@ import java.time.LocalTime;
 public class ItemRequest {
     private Long locationId;
     private Integer position; // position이 null일 경우 자동으로 할당
+    @JsonDeserialize(using = FlexibleLocalTimeDeserializer.class)
     private LocalTime startTime;
+    @JsonDeserialize(using = FlexibleLocalTimeDeserializer.class)
     private LocalTime endTime;
     private LocalDate date;
 }
