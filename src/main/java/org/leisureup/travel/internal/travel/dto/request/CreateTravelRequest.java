b4ffd4b64cf1db1f04b8ec06249beb5a683669d7
bbungjin <leejintwo12@naver.com>
2025-08-23 04:44:23 +0900

    feat: 여행 생성 역직렬화

--- src/main/java/org/leisureup/travel/internal/travel/dto/request/CreateTravelRequest.java
+++ src/main/java/org/leisureup/travel/internal/travel/dto/request/CreateTravelRequest.java
@@ -1,8 +1,10 @@
 package org.leisureup.travel.internal.travel.dto.request;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.leisureup.global.json.FlexibleLocalDateSerializer;
 import org.leisureup.travel.internal.travel.domain.Travel;
 
 import java.time.LocalDate;
@@ -14,7 +16,9 @@ import java.util.List;
 public class CreateTravelRequest {
     private String travelName;
     private String travelDescription;
+    @JsonSerialize(using = FlexibleLocalDateSerializer.class)
     private LocalDate startDate;
+    @JsonSerialize(using = FlexibleLocalDateSerializer.class)
     private LocalDate endDate;
     private List<ItemRequest> items;
     
