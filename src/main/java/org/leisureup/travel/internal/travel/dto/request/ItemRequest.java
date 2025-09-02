a89459386b079931b10fdf84114c419f8c7541df
청주는사과아님 <jbw9964@gmail.com>
2025-09-02 20:01:05 +0900

    hotfix: TravelController endpoint 수정
    - validation 추가
    - 빠진 `@RequestBody` 추가

--- src/main/java/org/leisureup/travel/internal/travel/dto/request/ItemRequest.java
+++ src/main/java/org/leisureup/travel/internal/travel/dto/request/ItemRequest.java
@@ -1,19 +1,18 @@
 package org.leisureup.travel.internal.travel.dto.request;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.leisureup.global.json.FlexibleLocalDateDeserializer;
-import org.leisureup.global.json.FlexibleLocalTimeDeserializer;
-
-import java.time.LocalDate;
-import java.time.LocalTime;
+import com.fasterxml.jackson.databind.annotation.*;
+import jakarta.validation.constraints.*;
+import java.time.*;
+import lombok.*;
+import org.leisureup.global.json.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class ItemRequest {
+
+    @NotNull
+    @Positive
     private Long locationId;
     private Integer position; // position이 null일 경우 자동으로 할당
     @JsonDeserialize(using = FlexibleLocalTimeDeserializer.class)
